04bf584a26847c8c357d15f0b42651d3f725b243
Bolesłsw Denk <boleslaw.denk@o2.pl>
2020-04-11 18:53:39 +0200

    [FIX]: junit tests

--- DesignPatterns/src/test/java/pl/codedesign/patterns/creational/builder/BicycleTest.java
+++ DesignPatterns/src/test/java/pl/codedesign/patterns/creational/builder/BicycleTest.java
@@ -14,7 +14,7 @@ public class BicycleTest {
 
 	@Test
 	public void test() {
-		Bicycle downhill = new Bicycle.Builder().breaks(Break.DISC).frame(Material.ALLUMINIUM).tires(Tire.OFFROAD)
+		Bicycle downhill = new BicycleBuilder().breaks(Break.DISC).frame(Material.ALLUMINIUM).tires(Tire.OFFROAD)
 				.suspensionBack(Suspension.OIL).suspensionFront(Suspension.OIL).build();
 
 		assertThat(Tire.OFFROAD).isEqualTo(downhill.getTires());
